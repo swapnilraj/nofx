aead021fa46cc77e96080243c157d9784f93d929
Swapnil Raj <raj.swapnil99@gmail.com>
2020-03-13 15:07:44 +0000

    Add carousel control with previous and next buttons

--- src/Components/Viz.purs
+++ src/Components/Viz.purs
@@ -2,10 +2,9 @@ module Components.Viz where
 
 import Prelude
 
-import Data.Array (fromFoldable)
-import Data.List (List(..), head, singleton, tail)
+import Data.List (List(..), head, init, last, singleton, tail)
 import Data.Maybe (Maybe(..), fromMaybe)
-import Data.Either (Either(..))
+import Data.Either (Either(..), hush)
 import Data.Traversable (sequence)
 
 import Compiler (GmState)
@@ -13,13 +12,12 @@ import Graphics (renderGmState)
 
 import Effect (Effect)
 import React.Basic.DOM as R
-import React.Basic.Hooks (ReactComponent, component, empty, element, useState, (/\))
+import React.Basic.Events (handler_)
+import React.Basic.Hooks (ReactComponent, component, empty, element, useState, useEffect, (/\))
 import React.Basic.Hooks as React
 import React.Basic.Hooks.Aff (useAff)
 
 import Global (encodeURIComponent)
-
-import Debug.Trace
 
 data Carousel a = Carousel (List a) a (List a)
 
@@ -34,30 +32,69 @@ nextCarousel (Carousel bef curr after) =
   where curr' = fromMaybe mempty $ head after
         tl = fromMaybe Nil $ tail after
 
+previousCarousel :: forall a.Monoid a => Carousel a -> Carousel a
+previousCarousel (Carousel bef curr after) =
+  Carousel ini lt (Cons curr after)
+  where ini = fromMaybe Nil $ init after
+        lt = fromMaybe mempty $ last bef
+
 emptyCarousel :: Carousel String
 emptyCarousel = (Carousel Nil "" Nil)
 
-mkViz :: Effect (ReactComponent { prog :: List GmState } )
-mkViz = do
-  carouseImg <- mkCarouselImg
-  component "Viz" \{ prog } -> React.do
-    carousel /\ setCarousel <- useState emptyCarousel
-    rendered <- useAff prog (sequence $ renderGmState <$> prog)
-    case rendered of
-        Just (Right imgs) -> do
-          let c = toCarousel $ mkImgSrc <$> imgs
-          _ <- pure $ setCarousel (\_ -> c)
-          _ <- trace imgs (\_ -> pure $ empty)
-          _ <- trace carousel (\_ -> pure $ empty)
-          _ <- trace (c) (\_ -> pure $ empty)
-          pure $ R.div_ [ element carouseImg { carousel:  carousel } ]
-        _ -> pure $ empty
-
 mkImgSrc :: String -> String
 mkImgSrc imgData =
-  "data:image/svg+xml;charset=utf8," <> (fromMaybe "" $ encodeURIComponent imgData)
+  "data:image/svg+xml;charset=utf8," <>
+  (fromMaybe "" $ encodeURIComponent imgData)
 
 mkCarouselImg :: Effect (ReactComponent { carousel :: Carousel String } )
 mkCarouselImg = component "CarouselImg" \{ carousel } -> React.do
                 let (Carousel _ c _) = carousel
                 pure $ R.img { src: c }
+
+mkViz :: Effect (ReactComponent { prog :: List GmState } )
+mkViz = do
+  carouselImg <- mkCarouselImg
+  carouselControl <- mkCarouselControl
+  component "Viz" \{ prog } -> React.do
+    carousel /\ setCarousel <- useState emptyCarousel
+    rendered <- useAff prog (sequence $ renderGmState <$> prog)
+    useEffect (rendered <#> hush) $ do
+      case rendered of
+        Just (Right imgs) -> do
+          setCarousel $ const $ toCarousel $ mkImgSrc <$> imgs
+        _ -> pure $ unit
+      pure $ mempty
+    case rendered of
+        Just (Right imgs) ->
+          pure $ R.div_ [ element carouselImg { carousel }
+                        , element carouselControl { carousel, setCarousel }
+                        ]
+        _ -> pure $ empty
+
+type CarouselAction = forall a. Monoid a => Effect (ReactComponent
+                        { carousel :: Carousel a
+                        , setCarousel :: (Carousel a -> Carousel a) -> Effect Unit
+                        })
+
+mkCarouselControl :: CarouselAction
+mkCarouselControl = do
+  nextButton <- mkNextButton
+  previousButton <- mkPreviousButton
+  component "CarouselControl" \props -> React.do
+     pure $ R.div_ [ element nextButton props
+                   , element previousButton props
+                   ]
+
+mkPreviousButton :: CarouselAction
+mkPreviousButton = component "PreviousButton" \{ carousel, setCarousel } -> React.do
+                let previous = previousCarousel carousel
+                pure $ R.button { children: [ R.text "Previous" ]
+                                , onClick: handler_ $ setCarousel $ const previous
+                                }
+
+mkNextButton :: CarouselAction
+mkNextButton = component "NextButton" \{ carousel, setCarousel } -> React.do
+                let next = nextCarousel carousel
+                pure $ R.button { children: [ R.text "Next" ]
+                                , onClick: handler_ $ setCarousel $ const next
+                                }
